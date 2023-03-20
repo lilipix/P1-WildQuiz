bb93c23a753a4d1b6548b4786a28a40295514f75
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-03-20 09:15:45 +0100

    Create the questions.js file with all the questions to the Quiz

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+//* Order of the questions
+//* 1questionSpecialiteCulinaire
+//* 2questionPlaceStatue
+//* 3questionPlaceArtise
+//* 4questionTheatreLyon
+//* 5quesionTraboule
+//* 6questionQuartierHistorique
+//* 7questionBougieFenetre
+//* 8questionQuartierDeCharme
+//* 9questionChefCuisinier
+//* 10questionDiscretMusee
+
+
+
+//* Object with all the questions and answers for the quiz.
+export const questionsToQuiz = {
+
+    questionSpecialiteCulinaire: {
+        question: "Quelle spécialité culinaire a fait la réputation de Lyon?",
+
+        answers: {
+            questionA: "La cervelle de canut",
+            questionB: "Le gratin de coquillettes au jambon",
+            questionC: "Les canelés",
+            questionD: "Le Donut’s, Hou pinaise c’est bon!"
+        }
+    },
+
+    questionPlaceStatue: {
+        question: "Sur la Place Bellecour trône la statue de ….? ",
+
+        answers: {
+            questionA: "Louis XIV sur son cheval",
+            questionB: "Jeanne d’Arc sur son cheval",
+            questionC: "Cyrano de Bergerac sur son cheval",
+            questionD: "Shrek sur son âne"
+        }
+    },
+
+    questionPlaceArtise: {
+        question: "L’artiste, auteur de cette statue, aurait oublié un détail important, lequel?",
+
+        answers: {
+            questionA: "La queue du cheval",
+            questionB: "Les étriers",
+            questionC: "La selle",
+            questionD: "Le bouclier de Captain America"
+        }
+    },
+
+    questionTheatreLyon: {
+        question: "De quel théâtre Lyon est-il le berceau?",
+
+        answers: {
+            questionA: "Le théâtre antique",
+            questionB: "Le théâtre de Guignol",
+            questionC: "La comedia del arte",
+            questionD: "Le théâtre de boulevard (il y a beaucoup de boulevards à Lyon, c’est pour ça)"
+        }
+    },
+
+    questionTraboule: {
+        question: "A Lyon si on vous propose de “prendre une traboule”… il faut comprendre : ",
+
+        answers: {
+            questionA: "Une expression pour dire “Casser la croûte”",
+            questionB: "On vous menace de prendre un poing dans la figure",
+            questionC: "On vous propose de passer par une petite ruelle située entre deux rues parallèles",
+            questionD: "C’est tellement obscène que je n’ose pas vous expliquer le sens de cette phrase…"
+        }
+    },
+
+    questionQuartierHistorique: {
+        question: "Comment s’appelle le quartier historique de Lyon, classé au patrimoinde de l’UNESCO?",
+
+        answers: {
+            questionA: "Saint-Thomas",
+            questionB: "Saint-Jean",
+            questionC: "Saint-Félicien",
+            questionD: "Saint-Tropez (charmant quartier typiquement lyonnais)"
+        }
+    },
+
+    questionBougieFenetre: {
+        question: "Pour quelle occasion les Lyonnais mettent des bougies à leur fenêtre le 8 décembre de chaque année?",
+
+        answers: {
+            questionA: "La Fête des Lumières",
+            questionB: "La Fête de la Saint-Jean",
+            questionC: "La Fête des Canuts",
+            questionD: "La grève annuelle d’EDF"
+        }
+    },
+
+    questionQuartierDeCharme: {
+        question: "Quartier de charme situé en haut d’une colline à Lyon et abritant autrefois les célèbres tisseurs lyonnais, je suis… ",
+
+        answers: {
+            questionA: "Le Point du Jour",
+            questionB: "La Croix-Rousse",
+            questionC: "Fourvière",
+            questionD: "Time Square"
+        }
+    },
+
+    questionChefCuisinier: {
+        question: "Quel chef cuisinier triplement étoilé et mondialement renommé était établi en région lyonnaise?",
+
+        answers: {
+            questionA: "Paul Bocuse",
+            questionB: "Cyril Lignac",
+            questionC: "Georges Blanc",
+            questionD: "Casimir (et son célèbre gloubiboulga)"
+        }
+    },
+
+    questionDiscretMusee: {
+        question: "Quel discret mais incontournable musée se situe en plein coeur du quartier Saint-Jean dans une maginifque bâtisse typiquement lyonnaise?",
+
+        answers: {
+            questionA: "Le musée d’Art Contemporain de Lyon",
+            questionB: "Le musée des Marionettes",
+            questionC: "Le musée de la Miniature et du Cinéma",
+            questionD: "Le musée de la Tour Eiffel (ils manquaient de place à Paris alors…)"
+        }
+    }
+}
