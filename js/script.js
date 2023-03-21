ea8309b914f0f2d597f7a186d255ba34a39b1d64
Teodor ZHEKOV <TeodorZhekov@Outlook.fr>
2023-03-21 14:09:44 +0100

    Added a click effect on the Play button at Page 1.

--- js/script.js
+++ js/script.js
@@ -213,7 +213,7 @@ function trueOrFalse(i) {
 
 }
 const playButton = document.querySelector(".play")
-const playButtonClick = <audio src="../assets/sound/play-button-click.mp3"></audio>
+const playButtonClick = document.querySelector("#play-button-click")
 
 playButton.addEventListener("click", () => {
   playButtonClick.play()
