d741c6c02ed3f4b83a4f49b95a21cf03d141ee4d
Teodor ZHEKOV <TeodorZhekov@Outlook.fr>
2023-03-21 12:10:28 +0100

    Added the background image for Home Page, added styles for it.

--- js/script.js
+++ js/script.js
@@ -212,3 +212,9 @@ function trueOrFalse(i) {
     }
 
 }
+const playButton = document.querySelector(".play")
+const playButtonClick = <audio src="../assets/sound/play-button-click.mp3"></audio>
+
+playButton.addEventListener("click", () => {
+  playButtonClick.play()
+})
