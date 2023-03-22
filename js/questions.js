8644046bfe0f71e545962de768590617289cab91
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-03-22 22:00:59 +0100

    Update the questions file

--- js/questions.js
+++ js/questions.js
@@ -19,21 +19,23 @@ export const questionsToQuiz = {
         question: "Quelle spécialité culinaire a fait la réputation de Lyon?",
 
         answers: {
-            questionA: "La cervelle de canut",
-            questionB: "Le gratin de coquillettes au jambon",
-            questionC: "Les canelés",
-            questionD: "Le Donut’s, Hou pinaise c’est bon!"
-        }
+            answerA: "La cervelle de canut",
+            answerB: "Le gratin de coquillettes au jambon",
+            answerC: "Les canelés",
+            answerD: "Le Donut’s, Hou pinaise c’est bon!"
+        },
+        
+        rightAnswer: "answerA"
     },
 
     questionPlaceStatue: {
         question: "Sur la Place Bellecour trône la statue de ….? ",
 
         answers: {
-            questionA: "Louis XIV sur son cheval",
-            questionB: "Jeanne d’Arc sur son cheval",
-            questionC: "Cyrano de Bergerac sur son cheval",
-            questionD: "Shrek sur son âne"
+            answerA: "Louis XIV sur son cheval",
+            answerB: "Jeanne d’Arc sur son cheval",
+            answerC: "Cyrano de Bergerac sur son cheval",
+            answerD: "Shrek sur son âne"
         }
     },
 
@@ -41,10 +43,10 @@ export const questionsToQuiz = {
         question: "L’artiste, auteur de cette statue, aurait oublié un détail important, lequel?",
 
         answers: {
-            questionA: "La queue du cheval",
-            questionB: "Les étriers",
-            questionC: "La selle",
-            questionD: "Le bouclier de Captain America"
+            answerA: "La queue du cheval",
+            answerB: "Les étriers",
+            answerC: "La selle",
+            answerD: "Le bouclier de Captain America"
         }
     },
 
@@ -52,10 +54,10 @@ export const questionsToQuiz = {
         question: "De quel théâtre Lyon est-il le berceau?",
 
         answers: {
-            questionA: "Le théâtre antique",
-            questionB: "Le théâtre de Guignol",
-            questionC: "La comedia del arte",
-            questionD: "Le théâtre de boulevard (il y a beaucoup de boulevards à Lyon, c’est pour ça)"
+            answerA: "Le théâtre antique",
+            answerB: "Le théâtre de Guignol",
+            answerC: "La comedia del arte",
+            answerD: "Le théâtre de boulevard (il y a beaucoup de boulevards à Lyon, c’est pour ça)"
         }
     },
 
@@ -63,10 +65,10 @@ export const questionsToQuiz = {
         question: "A Lyon si on vous propose de “prendre une traboule”… il faut comprendre : ",
 
         answers: {
-            questionA: "Une expression pour dire “Casser la croûte”",
-            questionB: "On vous menace de prendre un poing dans la figure",
-            questionC: "On vous propose de passer par une petite ruelle située entre deux rues parallèles",
-            questionD: "C’est tellement obscène que je n’ose pas vous expliquer le sens de cette phrase…"
+            answerA: "Une expression pour dire “Casser la croûte”",
+            answerB: "On vous menace de prendre un poing dans la figure",
+            answerC: "On vous propose de passer par une petite ruelle située entre deux rues parallèles",
+            answerD: "C’est tellement obscène que je n’ose pas vous expliquer le sens de cette phrase…"
         }
     },
 
@@ -74,10 +76,10 @@ export const questionsToQuiz = {
         question: "Comment s’appelle le quartier historique de Lyon, classé au patrimoinde de l’UNESCO?",
 
         answers: {
-            questionA: "Saint-Thomas",
-            questionB: "Saint-Jean",
-            questionC: "Saint-Félicien",
-            questionD: "Saint-Tropez (charmant quartier typiquement lyonnais)"
+            answerA: "Saint-Thomas",
+            answerB: "Saint-Jean",
+            answerC: "Saint-Félicien",
+            answerD: "Saint-Tropez (charmant quartier typiquement lyonnais)"
         }
     },
 
@@ -85,10 +87,10 @@ export const questionsToQuiz = {
         question: "Pour quelle occasion les Lyonnais mettent des bougies à leur fenêtre le 8 décembre de chaque année?",
 
         answers: {
-            questionA: "La Fête des Lumières",
-            questionB: "La Fête de la Saint-Jean",
-            questionC: "La Fête des Canuts",
-            questionD: "La grève annuelle d’EDF"
+            answerA: "La Fête des Lumières",
+            answerB: "La Fête de la Saint-Jean",
+            answerC: "La Fête des Canuts",
+            answerD: "La grève annuelle d’EDF"
         }
     },
 
@@ -96,10 +98,10 @@ export const questionsToQuiz = {
         question: "Quartier de charme situé en haut d’une colline à Lyon et abritant autrefois les célèbres tisseurs lyonnais, je suis… ",
 
         answers: {
-            questionA: "Le Point du Jour",
-            questionB: "La Croix-Rousse",
-            questionC: "Fourvière",
-            questionD: "Time Square"
+            answerA: "Le Point du Jour",
+            answerB: "La Croix-Rousse",
+            answerC: "Fourvière",
+            answerD: "Time Square"
         }
     },
 
@@ -107,10 +109,10 @@ export const questionsToQuiz = {
         question: "Quel chef cuisinier triplement étoilé et mondialement renommé était établi en région lyonnaise?",
 
         answers: {
-            questionA: "Paul Bocuse",
-            questionB: "Cyril Lignac",
-            questionC: "Georges Blanc",
-            questionD: "Casimir (et son célèbre gloubiboulga)"
+            answerA: "Paul Bocuse",
+            answerB: "Cyril Lignac",
+            answerC: "Georges Blanc",
+            answerD: "Casimir (et son célèbre gloubiboulga)"
         }
     },
 
@@ -118,10 +120,10 @@ export const questionsToQuiz = {
         question: "Quel discret mais incontournable musée se situe en plein coeur du quartier Saint-Jean dans une maginifque bâtisse typiquement lyonnaise?",
 
         answers: {
-            questionA: "Le musée d’Art Contemporain de Lyon",
-            questionB: "Le musée des Marionettes",
-            questionC: "Le musée de la Miniature et du Cinéma",
-            questionD: "Le musée de la Tour Eiffel (ils manquaient de place à Paris alors…)"
+            answerA: "Le musée d’Art Contemporain de Lyon",
+            answerB: "Le musée des Marionettes",
+            answerC: "Le musée de la Miniature et du Cinéma",
+            answerD: "Le musée de la Tour Eiffel (ils manquaient de place à Paris alors…)"
         }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import { questionsToQuiz } from "./questions.js";
 
 
 //carte leaflet
@@ -6,11 +7,11 @@ const map = L.map('map').setView([45.76952, 4.83652], 13);
 const modal = document.querySelector("section");
 const button = document.querySelector(".exitButton");
 const answers = document.querySelector(".answers");
-const ansA = document.querySelector(".ansA");
-const ansB = document.querySelector(".ansB");
-const ansC = document.querySelector(".ansC");
-const ansD = document.querySelector(".ansD");
-const answer = document.querySelectorAll["answer"];
+const answerA = document.querySelector(".answerA");
+const answerB = document.querySelector(".answerB");
+const answerC = document.querySelector(".answerC");
+const answserD = document.querySelector(".answerD");
+
 
 const instructions = document.querySelector("#instructions");
 
@@ -22,185 +23,92 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
+
 //marqueurs
 
+// let marker1 = { 
+//     coordinates : L.marker([45.76276, 4.84719]).addTo(map),
+//     questionSpecialiteCulinaire: {
+//         question: "Quelle spécialité culinaire a fait la réputation de Lyon?",
+
+//         answers: {
+//             answerA: "La cervelle de canut",
+//             answerB: "Le gratin de coquillettes au jambon",
+//             answerC: "Les canelés",
+//             answerD: "Le Donut’s, Hou pinaise c’est bon!"
+//         },
+        
+//         rightAnswer: "answerA"
+//     },
+// }
+
+let marker1 = L.marker([45.76276, 4.84719]).addTo(map);
+let marker2 = L.marker([45.75786, 4.83218]).addTo(map);
+let marker3 = L.marker([45.75751, 4.83355]).addTo(map);
+let marker4 = L.marker([45.76577, 4.82861]).addTo(map);
+let marker5 = L.marker([45.76284, 4.82779]).addTo(map);
+let marker6 = L.marker([45.76038, 4.82669]).addTo(map);
+let marker7 = L.marker([45.77835, 4.85344]).addTo(map);
+let marker8 = L.marker([45.77948, 4.82838]).addTo(map);
+let marker9 = L.marker([45.76353, 4.85087]).addTo(map);
+let marker10 = L.marker([45.76191, 4.82724]).addTo(map);
+
+let markers = [marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8, marker9, marker10 ]
 
 
-//questions
-const questionsToQuiz = {
 
-    questionSpecialiteCulinaire: {
-        question: "Quelle spécialité culinaire a fait la réputation de Lyon?",
-
-        answers: {
-            answerA: "La cervelle de canut",
-            answerB: "Le gratin de coquillettes au jambon",
-            answerC: "Les canelés",
-            answerD: "Le Donut’s, Hou pinaise c’est bon!"
-        },
-        rightAnswer: "answerA"
-    },
-
-    questionPlaceStatue: {
-        question: "Sur la Place Bellecour trône la statue de ….? ",
-
-        answers: {
-            answerA: "Louis XIV sur son cheval",
-            answerB: "Jeanne d’Arc sur son cheval",
-            answerC: "Cyrano de Bergerac sur son cheval",
-            answerD: "Shrek sur son âne"
-        }
-    },
-
-    questionPlaceArtise: {
-        question: "L’artiste, auteur de cette statue, aurait oublié un détail important, lequel?",
-
-        answers: {
-            answerA: "La queue du cheval",
-            answerB: "Les étriers",
-            answerC: "La selle",
-            answerD: "Le bouclier de Captain America"
-        }
-    },
-
-    questionTheatreLyon: {
-        question: "De quel théâtre Lyon est-il le berceau?",
-
-        answers: {
-            answerA: "Le théâtre antique",
-            answerB: "Le théâtre de Guignol",
-            answerC: "La comedia del arte",
-            answerD: "Le théâtre de boulevard (il y a beaucoup de boulevards à Lyon, c’est pour ça)"
-        }
-    },
-
-    questionTraboule: {
-        question: "A Lyon si on vous propose de “prendre une traboule”… il faut comprendre : ",
-
-        answers: {
-            answerA: "Une expression pour dire “Casser la croûte”",
-            answerB: "On vous menace de prendre un poing dans la figure",
-            answerC: "On vous propose de passer par une petite ruelle située entre deux rues parallèles",
-            answerD: "C’est tellement obscène que je n’ose pas vous expliquer le sens de cette phrase…"
-        }
-    },
-
-    questionQuartierHistorique: {
-        question: "Comment s’appelle le quartier historique de Lyon, classé au patrimoinde de l’UNESCO?",
-
-        answers: {
-            answerA: "Saint-Thomas",
-            answerB: "Saint-Jean",
-            answerC: "Saint-Félicien",
-            answerD: "Saint-Tropez (charmant quartier typiquement lyonnais)"
-        }
-    },
-
-    questionBougieFenetre: {
-        question: "Pour quelle occasion les Lyonnais mettent des bougies à leur fenêtre le 8 décembre de chaque année?",
-
-        answers: {
-            answerA: "La Fête des Lumières",
-            answerB: "La Fête de la Saint-Jean",
-            answerC: "La Fête des Canuts",
-            answerD: "La grève annuelle d’EDF"
-        }
-    },
-
-    questionQuartierDeCharme: {
-        question: "Quartier de charme situé en haut d’une colline à Lyon et abritant autrefois les célèbres tisseurs lyonnais, je suis… ",
-
-        answers: {
-            answerA: "Le Point du Jour",
-            answerB: "La Croix-Rousse",
-            answerC: "Fourvière",
-            answerD: "Time Square"
-        }
-    },
-
-    questionChefCuisinier: {
-        question: "Quel chef cuisinier triplement étoilé et mondialement renommé était établi en région lyonnaise?",
-
-        answers: {
-            answerA: "Paul Bocuse",
-            answerB: "Cyril Lignac",
-            answerC: "Georges Blanc",
-            answerD: "Casimir (et son célèbre gloubiboulga)"
-        }
-    },
-
-    questionDiscretMusee: {
-        question: "Quel discret mais incontournable musée se situe en plein coeur du quartier Saint-Jean dans une maginifque bâtisse typiquement lyonnaise?",
-
-        answers: {
-            answerA: "Le musée d’Art Contemporain de Lyon",
-            answerB: "Le musée des Marionettes",
-            answerC: "Le musée de la Miniature et du Cinéma",
-            answerD: "Le musée de la Tour Eiffel (ils manquaient de place à Paris alors…)"
-        }
-    }
-}
-
-
-let marker1 = [L.marker([45.76276, 4.84719]).addTo(map), questionsToQuiz.questionSpecialiteCulinaire]
-let marker2 = [L.marker([45.75786, 4.83218]).addTo(map)]
-let marker3 = [L.marker([45.75751, 4.83355]).addTo(map)]
-let marker4 = [L.marker([45.76577, 4.82861]).addTo(map)]
-let marker5 = [L.marker([45.76284, 4.82779]).addTo(map)]
-let marker6 = [L.marker([45.76038, 4.82669]).addTo(map)]
-let marker7 = [L.marker([45.77835, 4.85344]).addTo(map)]
-let marker8 = [L.marker([45.77948, 4.82838]).addTo(map)]
-let marker9 = [L.marker([45.76353, 4.85087]).addTo(map)]
-let marker10 = [L.marker([45.76191, 4.82724]).addTo(map)]
-
-let markers = { marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8, marker9, marker10 }
-console.log(markers)
-
-//markers.forEach(marker => marker.addEventListener("click", handleClick))
-markers[0].addEventListener("click", handleClick)
 
 
 // fonction pour afficher le modal avec les questions
-//import { questionsToQuiz } from './questions.js'
-//console.log(questionsToQuiz)
 
 
-//console.log(questionsToQuiz.questionSpecialiteCulinaire.answers.answerA[1])
-function handleClick(event) {
+
+function handleClick(event, marker) {
     instructions.style.display = "none";
-    console.log(marker1)
+    
 
-    if (event.latlng.lat === 45.76276 && event.latlng.lng === 4.84719) {
-        let modalPopUp = document.createElement("div")
-        modalPopUp.innerHTML =
+    // if (event.latlng.lat === 45.76276 && event.latlng.lng === 4.84719) {
+    //     let modalPopUp = document.createElement("div")
+    //     modalPopUp.innerHTML =
+    //     `<div class="modal">
+    //         <h2 class="modalQuestion">${questionsToQuiz.questionSpecialiteCulinaire.question}</h2>
+    //         <p class="exitButton">Suivant</p>
+    //         <div class="answers">
+    //         <label class="answer answerA" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerA})">
+    //             <div class="choix">A</div>
+    //         </label>
+    //         <label class="answer answerB" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerB})">
+    //             <div class="choix">B</div>
+    //         </label>
+    //         <label class="answer answerC" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerC})">
+    //             <div class="choix">C</div>
+    //         </label>
+    //         <label class="answer answerD" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerD})">
+    //             <div class="choix">D</div>
+    //         </label>
+    //     </div>
+    //         <!--timer
+    //         <div id="timer"></div>
+    //         <script src="timer.js"></script>-->
+    //         </div>
+    //     `
 
-            `<div class="modal">
-    <h2 class="modalQuestion">${questionsToQuiz.questionSpecialiteCulinaire.question}</h2>
-    <p class="exitButton">Suivant</p>
-    <div class="answers">
-    <label class="answer ansA" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerA})">
-        <div class="choix">A</div>
-        </label>
-    <label class="answer ansB" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerB})">
-        <div class="choix">B</div>
-    </label>
-    <label class="answer ansC" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerC})">
-        <div class="choix">C</div>
-    </label>
-    <label class="answer ansD" onclick="trueOrFalse(${questionsToQuiz.questionSpecialiteCulinaire.answers.answerD})">
-        <div class="choix">D</div>
-    </label>
-    </div>
-<!--timer
-    <div id="timer"></div>
-    <script src="timer.js"></script>-->
-    </div>`
-
-        modal.appendChild(modalPopUp)
-
-    }
+    //     modal.appendChild(modalPopUp)
+        
+    // }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 function trueOrFalse(i) {
     let element = document.querySelectorAll(".answers")
