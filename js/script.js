9f64a4a16df84f7539b4a396a8a3ac3284a12c49
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-03-23 20:38:07 +0100

    Cleaning and starting over

--- js/script.js
+++ js/script.js
@@ -21,8 +21,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //* Creating the class with parameters necessary t create a modal
 class Marker {
-    constructor(coordinate, question, answerA, answerB, answerC, answerD, rightAnswer) {
-        this.coordinate = coordinate;
+    constructor(question, answerA, answerB, answerC, answerD, rightAnswer) {
         this.question = question;
         this.answerA = answerA;
         this.answerB = answerB;
@@ -34,8 +33,7 @@ class Marker {
 }
 
 //* Contructing all classes necessary fo the quiz 
-const marker1 = new Marker(
-    L.marker([45.76276, 4.84719]).addTo(map), 
+const pare1 = new Marker(
     questionsToQuiz.questionSpecialiteCulinaire.question, 
     questionsToQuiz.questionSpecialiteCulinaire.answers.answerA, 
     questionsToQuiz.questionSpecialiteCulinaire.answers.answerB, 
@@ -44,8 +42,7 @@ const marker1 = new Marker(
     questionsToQuiz.questionSpecialiteCulinaire.rightAnswer 
 )
 
-const marker2 = new Marker(
-    L.marker([45.75786, 4.83218]).addTo(map), 
+const pare2 = new Marker(
     questionsToQuiz.questionPlaceStatue.question, 
     questionsToQuiz.questionPlaceStatue.answers.answerA, 
     questionsToQuiz.questionPlaceStatue.answers.answerB, 
@@ -54,8 +51,7 @@ const marker2 = new Marker(
     questionsToQuiz.questionPlaceStatue.rightAnswer 
 )
 
-const marker3 = new Marker(
-    L.marker([45.75751, 4.83355]).addTo(map), 
+const pare3 = new Marker(
     questionsToQuiz.questionPlaceArtise.question, 
     questionsToQuiz.questionPlaceArtise.answers.answerA, 
     questionsToQuiz.questionPlaceArtise.answers.answerB, 
@@ -64,8 +60,7 @@ const marker3 = new Marker(
     questionsToQuiz.questionPlaceArtise.rightAnswer 
 )
 
-const marker4 = new Marker(
-    L.marker([45.76577, 4.82861]).addTo(map), 
+const pare4 = new Marker(
     questionsToQuiz.questionTheatreLyon.question, 
     questionsToQuiz.questionTheatreLyon.answers.answerA, 
     questionsToQuiz.questionTheatreLyon.answers.answerB, 
@@ -74,8 +69,7 @@ const marker4 = new Marker(
     questionsToQuiz.questionTheatreLyon.rightAnswer 
 )
 
-const marker5 = new Marker(
-    L.marker([45.76284, 4.82779]).addTo(map), 
+const pare5 = new Marker(
     questionsToQuiz.questionTraboule.question, 
     questionsToQuiz.questionTraboule.answers.answerA, 
     questionsToQuiz.questionTraboule.answers.answerB, 
@@ -84,8 +78,7 @@ const marker5 = new Marker(
     questionsToQuiz.questionTraboule.rightAnswer 
 )
 
-const marker6 = new Marker(
-    L.marker([45.76038, 4.82669]).addTo(map), 
+const pare6 = new Marker(
     questionsToQuiz.questionQuartierHistorique.question, 
     questionsToQuiz.questionQuartierHistorique.answers.answerA, 
     questionsToQuiz.questionQuartierHistorique.answers.answerB, 
@@ -94,8 +87,7 @@ const marker6 = new Marker(
     questionsToQuiz.questionQuartierHistorique.rightAnswer 
 )
 
-const marker7 = new Marker(
-    L.marker([45.77835, 4.85344]).addTo(map), 
+const pare7 = new Marker(
     questionsToQuiz.questionBougieFenetre.question, 
     questionsToQuiz.questionBougieFenetre.answers.answerA, 
     questionsToQuiz.questionBougieFenetre.answers.answerB, 
@@ -104,8 +96,7 @@ const marker7 = new Marker(
     questionsToQuiz.questionBougieFenetre.rightAnswer 
 )
 
-const marker8 = new Marker(
-    L.marker([45.77948, 4.82838]).addTo(map), 
+const pare8 = new Marker(
     questionsToQuiz.questionQuartierDeCharme.question, 
     questionsToQuiz.questionQuartierDeCharme.answers.answerA, 
     questionsToQuiz.questionQuartierDeCharme.answers.answerB, 
@@ -114,8 +105,8 @@ const marker8 = new Marker(
     questionsToQuiz.questionQuartierDeCharme.rightAnswer 
 )
 
-const marker9 = new Marker(
-    L.marker([45.76353, 4.85087]).addTo(map), 
+const pare9 = new Marker(
+    
     questionsToQuiz.questionChefCuisinier.question, 
     questionsToQuiz.questionChefCuisinier.answers.answerA, 
     questionsToQuiz.questionChefCuisinier.answers.answerB, 
@@ -124,8 +115,7 @@ const marker9 = new Marker(
     questionsToQuiz.questionChefCuisinier.rightAnswer 
 )
 
-const marker10 = new Marker(
-    L.marker([45.76191, 4.82724]).addTo(map), 
+const pare10 = new Marker( 
     questionsToQuiz.questionDiscretMusee.question, 
     questionsToQuiz.questionDiscretMusee.answers.answerA, 
     questionsToQuiz.questionDiscretMusee.answers.answerB, 
@@ -134,52 +124,98 @@ const marker10 = new Marker(
     questionsToQuiz.questionDiscretMusee.rightAnswer 
 )
 
-let markers = [marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8, marker9, marker10]
 
-markers.forEach(element => {
-    element.coordinate.addEventListener("click", handleClick(event, element))
-});
 
-// markers.coordinate.addEventListener("click", () => {alert("hello")})
-// marker2.coordinate.addEventListener("click", handleClick(marker2))
-// marker3.coordinate.addEventListener("click", handleClick(marker3))
-// marker4.coordinate.addEventListener("click", handleClick(marker4))
-// markers.forEach( mark => { console.log(mark.coordinate)})
+
+let marker1 = L.marker([45.76276, 4.84719]).addTo(map);
+let marker2 = L.marker([45.75786, 4.83218]).addTo(map);
+let marker3 = L.marker([45.75751, 4.83355]).addTo(map);
+let marker4 = L.marker([45.76577, 4.82861]).addTo(map);
+let marker5 = L.marker([45.76284, 4.82779]).addTo(map);
+let marker6 = L.marker([45.76038, 4.82669]).addTo(map);
+let marker7 = L.marker([45.77835, 4.85344]).addTo(map);
+let marker8 = L.marker([45.77948, 4.82838]).addTo(map);
+let marker9 = L.marker([45.76353, 4.85087]).addTo(map);
+let marker10 = L.marker([45.76191, 4.82724]).addTo(map);
+
+// let markers = [marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8, marker9, marker10]
+
+// markers.forEach(marker => marker.addEventListener("click", something))
+
+// marker1.addEventListener('click', handleClick(marker1));
+// marker1.addEventListener('click', handleClick())
+
+const popup = L.popup();
+marker1.on('click', onMapClick);
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(`<div class="modal">
+        <h2 class="modalQuestion">${pare1.question}</h2>
+        <p class="exitButton">Suivant</p>
+        <div class="answers">
+        <label class="answer answerA" onclick="trueOrFalse()">
+            <div class="choix">A</div>${pare1.answerA}
+        </label>
+        <label class="answer answerB" onclick="trueOrFalse()">
+            <div class="choix">B</div>${pare1.answerB}
+        </label>
+        <label class="answer answerC" onclick="trueOrFalse()">
+            <div class="choix">C</div>${pare1.answerC}
+        </label>
+        <label class="answer answerD" onclick="trueOrFalse()">
+            <div class="choix">D</div>${pare1.answerD}
+        </label>
+    </div>
+        <!--timer
+        <div id="timer"></div>
+        <script src="timer.js"></script>-->
+        </div>
+    `)
+        .openOn(map);
+}
+
+
+
+
+
+
 
 
 //* Function to display the modal with right class that was previos decalred
 
-function handleClick(marker) {
-    instructions.style.display = "none";
+// function handleClick(marker) {
+   
+//     instructions.style.display = "none";
+//     let modalPopUp = document.createElement("div")
+//         modalPopUp.innerHTML =
+//         `<div class="modal">
+//             <h2 class="modalQuestion">${marker.question}</h2>
+//             <p class="exitButton">Suivant</p>
+//             <div class="answers">
+//             <label class="answer answerA" onclick="trueOrFalse()">
+//                 <div class="choix">A</div>${marker.answerA}
+//             </label>
+//             <label class="answer answerB" onclick="trueOrFalse()">
+//                 <div class="choix">B</div>${marker.answerB}
+//             </label>
+//             <label class="answer answerC" onclick="trueOrFalse()">
+//                 <div class="choix">C</div>${marker.answerC}
+//             </label>
+//             <label class="answer answerD" onclick="trueOrFalse()">
+//                 <div class="choix">D</div>${marker.answerD}
+//             </label>
+//         </div>
+//             <!--timer
+//             <div id="timer"></div>
+//             <script src="timer.js"></script>-->
+//             </div>
+//         `
+//     modal.appendChild(modalPopUp)
+//     // marker.question
     
-    let modalPopUp = document.createElement("div")
-        modalPopUp.innerHTML =
-        `<div class="modal">
-            <h2 class="modalQuestion">${marker.question}</h2>
-            <p class="exitButton">Suivant</p>
-            <div class="answers">
-            <label class="answer answerA" onclick="trueOrFalse()">
-                <div class="choix">A</div>${marker.answerA}
-            </label>
-            <label class="answer answerB" onclick="trueOrFalse()">
-                <div class="choix">B</div>${marker.answerB}
-            </label>
-            <label class="answer answerC" onclick="trueOrFalse()">
-                <div class="choix">C</div>${marker.answerC}
-            </label>
-            <label class="answer answerD" onclick="trueOrFalse()">
-                <div class="choix">D</div>${marker.answerD}
-            </label>
-        </div>
-            <!--timer
-            <div id="timer"></div>
-            <script src="timer.js"></script>-->
-            </div>
-        `
-    modal.appendChild(modalPopUp)
-    // marker.question
-    
-}
+// }
 
 
 // function trueOrFalse(i) {
